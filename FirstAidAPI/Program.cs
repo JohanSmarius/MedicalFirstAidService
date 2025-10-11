c139ba4b85616202ec6ed6a3571a9c2bd5a616ed
Johan Smarius <jsmarius@hotmail.com>
2025-10-11 17:43:01 +0200

    Removed old projects

--- FirstAidAPI/Program.cs
+++ FirstAidAPI/Program.cs
@@ -1,8 +1,5 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Domain;
-using DomainService;
-using Infrastructure;
 using Staff.API;
 using Event.API;
 
@@ -11,10 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
 
 builder.AddStaffModule();
 builder.AddEventModule();
